60779cc52656c7e7a4f696218fe37812c710a275
neuraloutlook <nerualoutlook@gmail.com>
2025-06-06 05:46:25 +0530

    Project

---
+++ Spring-Essentials/src/test/java/com/AIChatBot/model/SpringEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AIChatBot.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
